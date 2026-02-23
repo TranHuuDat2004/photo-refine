614e6f4e3282fb59224baeada91f00a0d43d33aa
Trần Hữu Đạt <522h0081@student.tdtu.edu.vn>
2026-02-23 13:21:57 +0700

    fix: explicit dotenv path resolution

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const path = require('path');
 
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
